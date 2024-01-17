917bf4a1d55d7b8e51981929cf53d95687512985
thuyanduong <thuyanannduong@gmail.com>
2024-01-17 01:37:58 -0500

    one line graphql endpoint

--- src/server.js
+++ src/server.js
@@ -8,13 +8,7 @@ const port = process.env.PORT || 5003
 
 app.use("/api", rootRouter)
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    graphiql: true,
-    schema: schema
-  })
-)
+app.use("/graphql", graphqlHTTP({ graphiql: true, schema: schema }))
 
 app.get("/", async (req, res) => {
   res.send("Sample Movies API")
