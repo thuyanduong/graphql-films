1efc2461ee2c729b3743729e548ee9adaf468749
thuyanduong <thuyanannduong@gmail.com>
2024-01-16 19:54:54 -0500

    finished code

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -6,15 +6,14 @@ import { CategoryType, categoriesResolver } from './category.graphql.js'
 const QueryType = `#graphql
   type Query {
     films(page: Int, pageSize: Int): [Film!]!
-    languages: [Language!]!
-    categories: [Category!]!
     film (id: ID!): Film
+    languages: [Language!]!
     language (id: ID!): Language
+    categories: [Category!]!
     category (id: ID!): Category
   }
   `
 
-//Step 3. define a schema
 const schema = makeExecutableSchema({
   typeDefs: [QueryType, FilmType, LanguageType, CategoryType],
   resolvers: [filmsResolver, languagesResolver, categoriesResolver]
--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import rootRouter from './routers/index.js'
-
-//Step 1. dependencies
 import { graphqlHTTP } from 'express-graphql'
 import schema from './graphql/schema.js'
 
@@ -10,7 +8,6 @@ const port = process.env.PORT || 5003
 
 app.use('/api', rootRouter)
 
-//Step 2. Endpoint and Web Browser GUI
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
   schema: schema,
